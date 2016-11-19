d7b101a330acfc5c27347201eeab478f469eb001
Oduntan Oluwatobiloba Stephen <stephen.oduntan@andela.com>
2016-11-19 09:09:50 +0100

    Updated index file

--- index.js
+++ index.js
@@ -4,13 +4,16 @@
   var express = require('express'),
     bodyParser = require('body-parser'),
     morgan = require('morgan'),
-    routes = require('./routes/user');
+    routes = require('./routes/user'),
     app = express();
 
-    app.use(routes);
+  app.use(routes);
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(bodyParser.json());
+  app.use(morgan('dev'));
 
-    app.listen(3001, function () {
-      console.log('Server started at port 3001');
-    });
+  app.listen(3001, function () {
+    console.log('Server started at port 3001');
+  });
 
 } ());
